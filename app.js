c7d5a1cbf31b17d9e869f8b08fcdda7f78de790d
spreet2472 <samre2472@gmail.com>
2020-09-30 11:30:50 -0400

    addedShowRoute

--- app.js
+++ app.js
@@ -54,6 +54,17 @@ app.post('/blogs', (req, res) => {
   });
 });
 
+// SHOW Route
+app.get('/blogs/:id', (req, res) => {
+  Blog.findById(req.params.id, (err, foundBlog) => {
+    if (err) {
+      res.redirect('/blogs');
+    } else {
+      res.render('show', { blog: foundBlog });
+    }
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
