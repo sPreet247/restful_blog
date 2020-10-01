f981914a9be3eba00bb5d52dc4c08f1b27199706
spreet2472 <samre2472@gmail.com>
2020-10-01 11:35:51 -0400

    edit&upAdded

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/rest_blog', {
   useNewUrlParser: true,
@@ -12,6 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
+app.use(methodOverride('_method'));
 
 //Adding a pattern or schema
 const blogSchema = new mongoose.Schema({
@@ -28,7 +30,7 @@ app.get('/', (req, res) => {
   res.redirect('/blogs');
 });
 
-// INDEX
+// INDEX Route
 app.get('/blogs', (req, res) => {
   Blog.find({}, (err, blogs) => {
     if (err) {
@@ -39,11 +41,11 @@ app.get('/blogs', (req, res) => {
   });
 });
 
-// NEW
+// NEW Route
 app.get('/new', (req, res) => {
   res.render('new');
 });
-// CREATE
+// CREATE Route
 app.post('/blogs', (req, res) => {
   Blog.create(req.body.blog, (err, newBlog) => {
     if (err) {
@@ -61,6 +63,39 @@ app.get('/blogs/:id', (req, res) => {
       res.redirect('/blogs');
     } else {
       res.render('show', { blog: foundBlog });
+    }
+  });
+});
+
+// EDIT Route
+app.get('/blogs/:id/edit', (req, res) => {
+  Blog.findById(req.params.id, (err, foundBlog) => {
+    if (err) {
+      res.redirect('/blogs');
+    } else {
+      res.render('edit', { blog: foundBlog });
+    }
+  });
+});
+
+// UPDATE Route
+app.put('/blogs/:id', (req, res) => {
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog) => {
+    if (err) {
+      res.redirect('/blogs');
+    } else {
+      res.redirect('/blogs/' + req.params.id);
+    }
+  });
+});
+
+// DELETE Route
+app.delete('/blogs/:id', (req, res) => {
+  Blog.findByIdAndRemove(req.params.id, (err) => {
+    if (err) {
+      res.redirect('/blogs');
+    } else {
+      res.redirect('/blogs');
     }
   });
 });
